2388965b3155a4c79abccb2f3535b5be7315b70f
TAIJULAMAN <taijulaman8@gmail.com>
2025-01-12 12:31:07 +0600

    fix issue of sidebar

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,19 +1,19 @@
-import AnalyticsInfo from "../../components/Dashboard/AnalyticsInfo"
-import EarningGrowth from "../../components/Dashboard/EarningGrowth"
-import SubscriptionGrowth from "../../components/Dashboard/SubscriptionGrowth"
-import UserGrowth from "../../components/Dashboard/UserGrowth"
+import AnalyticsInfo from "../../components/Dashboard/AnalyticsInfo";
+import EarningGrowth from "../../components/Dashboard/EarningGrowth";
+import SubscriptionGrowth from "../../components/Dashboard/SubscriptionGrowth";
+import UserGrowth from "../../components/Dashboard/UserGrowth";
 
 function DashboardPage() {
-    return (
-        <div>
-        <AnalyticsInfo />
-        <div className="grid grid-cols-1 md:grid-cols-1 mmd:grid-cols-1 lg:grid-cols-2 gap-5">
-            <UserGrowth />
-            <SubscriptionGrowth />
-        </div>
-        <EarningGrowth />
+  return (
+    <div>
+      <AnalyticsInfo />
+      <div className="grid grid-cols-1 md:grid-cols-1 mmd:grid-cols-1 lg:grid-cols-2 gap-5">
+        <UserGrowth />
+        <SubscriptionGrowth />
+      </div>
+      <EarningGrowth />
     </div>
-    )
+  );
 }
 
-export default DashboardPage
+export default DashboardPage;
