eccd5f416edd304dc74224a96e928c68b8143dca
TAIJULAMAN <taijulaman8@gmail.com>
2025-02-06 11:08:47 +0600

    analytic and user growth completed

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -19,5 +19,5 @@ export const baseApi = createApi({
     },
   }),
   endpoints: () => ({}),
-  tagTypes: ["admin", "services", "technician", "supervisor","client"],
+  tagTypes: ["admin", "services", "technician", "supervisor","client", "dashboard"],
 });
