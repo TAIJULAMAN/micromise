082b1c04f379eb479c6c44000a11351d267d7bb8
TAIJULAMAN <taijulaman8@gmail.com>
2025-02-19 12:12:54 +0600

    user management completed completed

--- src/pages/user-management/Technician.jsx
+++ src/pages/user-management/Technician.jsx
@@ -16,26 +16,27 @@ function Technician() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
   const [currentRecord, setCurrentRecord] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
   // ............................................................
+  const [currentPage, setCurrentPage] = useState(1);
   const query = { isDeleted: false, page: currentPage };
   const [searchTerm, setSearchTerm] = useState("");
   const debouncedSearchTerm = useDebounced({
     searchTerm: searchTerm,
     delay: 500,
   });
-  console.log(debouncedSearchTerm, " debouncedSearchTerm");
-  query["searchTerm"] = debouncedSearchTerm;
-  // eslint-disable-next-line no-extra-boolean-cast
-  // if (!!debouncedSearchTerm) {
-  //     query["searchTerm"] = debouncedSearchTerm;
-  // }
+
+  // console.log("add er age", query);
+  query.searchTerm = debouncedSearchTerm;
+  // console.log("add er pore", query);
+
   const handleSearch = (e) => {
     const searchText = e.target.value;
-    // console.log(searchText,"searchText")
     setSearchTerm(searchText);
   };
 
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
   // ...................................................................
 
   // fetch all technician
@@ -45,11 +46,6 @@ function Technician() {
     isLoading,
     refetch,
   } = useGetAllTechnicianQuery(query);
-  // console.log(technicianData);
-
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
 
   // delete Data
   const [deleteTechnician] = useDeleteTechnicianMutation();
