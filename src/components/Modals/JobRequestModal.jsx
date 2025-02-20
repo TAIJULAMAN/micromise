2cffcd5f224d2c28f625e5c1e1a83b90438b7a5e
TAIJULAMAN <taijulaman8@gmail.com>
2025-02-20 11:07:58 +0600

    job req view modal completed

--- src/components/Modals/JobRequestModal.jsx
+++ src/components/Modals/JobRequestModal.jsx
@@ -1,24 +1,36 @@
 import { IoCloseSharp } from "react-icons/io5";
-import img from "/table.png";
 
-function JobRequestModal({ setRequestModal }) {
+function JobRequestModal({ setRequestModal, currentRecord }) {
+  console.log(currentRecord);
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-2">
       <div className="bg-white w-[400px] rounded-lg shadow-lg overflow-hidden">
         {/* Header Section */}
         <div className="bg-secondary pt-5 pb-2 text-center relative">
           <div className="w-20 h-20 mx-auto rounded-full overflow-hidden">
-            <img src={img} alt="Technician Profile" className="w-full h-full" />
+            <div className="flex gap-2 justify-start items-center">
+              <img
+                src={
+                  currentRecord?.userId?.profileImg
+                    ? `${currentRecord?.userId?.profileImg}`
+                    : "https://avatar.iran.liara.run/public/44"
+                }
+                alt={currentRecord?.userId?._id || "Anonymous User"}
+                className="h-20 w-20 rounded-full object-cover"
+              />
+            </div>
           </div>
-          <h2 className="text-md font-bold mt-2 text-white">Dindiniya</h2>
-          <h2 className="text-sm mt-1 text-gray-600">Admin Client</h2>
-             {/* Close Button */}
-                    <button
-                      onClick={() => setRequestModal(false)}
-                      className="absolute top-2 right-2 text-white bg-primary focus:outline-none p-2 rounded-full"
-                    >
-                      <IoCloseSharp />
-                    </button>
+          <h2 className="text-md font-bold mt-2 text-white">
+            {currentRecord?.userId?.fullName}
+          </h2>
+          <h2 className="text-sm mt-1 text-gray-600">Client</h2>
+          {/* Close Button */}
+          <button
+            onClick={() => setRequestModal(false)}
+            className="absolute top-2 right-2 text-white bg-primary focus:outline-none p-2 rounded-full"
+          >
+            <IoCloseSharp />
+          </button>
         </div>
 
         {/* Details Section */}
@@ -29,36 +41,39 @@ function JobRequestModal({ setRequestModal }) {
           <div className="space-y-2">
             <div className="flex flex-col space-y-2">
               <span className="font-medium text-[#171717]">User Name</span>
-              <span className="text-[#707070]">Dindiniya</span>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <span className="font-medium text-[#171717]">Reg No</span>
-              <span className="text-[#707070]">N64849458</span>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <span className="font-medium text-[#171717]">Contact</span>
-              <span className="text-[#707070]">(201)64889574</span>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <span className="font-medium text-[#171717]">Location</span>
               <span className="text-[#707070]">
-                68/ Joker Vila, Gotham City
+                {" "}
+                {currentRecord?.userId?.fullName}
               </span>
             </div>
             <div className="flex flex-col space-y-2">
-              <span className="font-medium text-[#171717]">Email</span>
-              <span className="text-[#707070]">mahmud@gmail.com</span>
+              <span className="font-medium text-[#171717]">Reg No</span>
+              <span className="text-[#707070]"> {currentRecord?.jobId}</span>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <span className="font-medium text-[#171717]">Date</span>
+              <span className="text-[#707070]">
+                {currentRecord?.createdAt
+                  ? new Date(currentRecord?.createdAt).toLocaleDateString(
+                      "en-US",
+                      {
+                        year: "numeric",
+                        month: "short",
+                        day: "2-digit",
+                      }
+                    )
+                  : "N/A"}
+              </span>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <span className="font-medium text-[#171717]">Services</span>
+              <span className="text-[#707070]">
+                {currentRecord?.services?.length
+                  ? currentRecord.services.join(", ")
+                  : "No services available"}
+              </span>
             </div>
           </div>
-
-          {/* <div className="mt-6 flex justify-center">
-            <button
-              onClick={() => setRequestModal(false)}
-              className="px-4 py-2 bg-primary text-white rounded"
-            >
-              Close
-            </button>
-          </div> */}
         </div>
       </div>
     </div>
